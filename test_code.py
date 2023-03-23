54ce1d5c744dc729fc6c8ab59fc97447fc674564
MaximeBSoma <125667579+MaximeBSoma@users.noreply.github.com>
2023-03-23 14:04:12 +0100

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import streamlit as st
 
-df = pd.read_excel('Staffing.xlsx')
+column_names = ['id_soma','lastName','firstName','town','profil','dispo','statut','numberOfResumes','diplome','commentaire','lien_CV','experience']
+
+
+df = pd.read_excel('Staffing.xlsx',header=1, skiprows= 7,index_col=0)
+
+st.header('Staffing')
 st.dataframe(data=df)
